5fe8fb86ac0aaa9fe5b3673cab4f41470b158bdc
nrastija <niko.rastija@gmail.com>
2024-12-18 16:05:34 +0100

    Implement Vue router solution for navigation bar and website routing

---
+++ src/views/Table.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="table-container">
+      <h1>Tablica Korisnika</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Ime</th>
+            <th>Prezime</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(korisnik, index) in korisnici" :key="index">
+            <td>{{ korisnik.id }}</td>
+            <td>{{ korisnik.ime }}</td>
+            <td>{{ korisnik.prezime }}</td>
+            <td>{{ korisnik.email }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        korisnici: [
+          { id: 1, ime: "Ivan", prezime: "Ivić", email: "ivan.ivic@example.com" },
+          { id: 2, ime: "Ana", prezime: "Anić", email: "ana.anic@example.com" },
+          { id: 3, ime: "Marko", prezime: "Markić", email: "marko.markic@example.com" },
+          { id: 4, ime: "Petra", prezime: "Petrić", email: "petra.petric@example.com" },
+        ],
+      };
+    },
+  };
+  </script>
